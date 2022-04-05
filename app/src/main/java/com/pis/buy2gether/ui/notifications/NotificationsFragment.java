12331cff2f4608ede8ae3ca574f8f5a1db311516
victorrjimenezz <vjimenru9@alumnes.ub.edu>
2022-04-05 20:32:21 +0200

    Defined Notifications RecyclerView List Adapter

--- app/src/main/java/com/pis/buy2gether/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/pis/buy2gether/ui/notifications/NotificationsFragment.java
@@ -4,18 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import com.pis.buy2gether.R;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.pis.buy2gether.databinding.FragmentNotificationsBinding;
 
-public class NotificationsFragment extends Fragment {
+import java.util.ArrayList;
+
+public class NotificationsFragment extends Fragment implements NotificationsListAdapter.ItemClickListener {
 
     private NotificationsViewModel notificationsViewModel;
+    private NotificationsListAdapter notificationsListAdapter;
     private FragmentNotificationsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -26,14 +30,51 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
+        ArrayList<String> items = new ArrayList<>();
+        items.add("Horse");
+        items.add("Cow");
+        items.add("Camel");
+        items.add("Sheep");
+        items.add("Chen");
+        items.add("Tula");
+        items.add("Chica");
+        items.add("Golfo");
+        items.add("Juanjo");
+        items.add("Tumama");
+        items.add("Goat");
+        items.add("Goat");
+        items.add("Goat");
+        items.add("Goat");
+        items.add("Goat");
+        items.add("Goat");
+        items.add("Goat");
+        items.add("Sheep");
+        items.add("Sheep");
+        items.add("Goat");
+        items.add("Goat");
+
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = binding.notificationsList;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        notificationsListAdapter = new NotificationsListAdapter(getContext(), items);
+        recyclerView.setAdapter(notificationsListAdapter);
+        notificationsListAdapter.setClickListener(this);
+        binding.notificationsList.setAdapter(notificationsListAdapter);
+
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                // data to populate the RecyclerView with
             }
+
         });
         return root;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getContext(), "You clicked " + notificationsListAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
     @Override
---
+++ app/src/main/java/com/pis/buy2gether/ui/notifications/NotificationsListAdapter.java
@@ -0,0 +1,86 @@
+package com.pis.buy2gether.ui.notifications;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.pis.buy2gether.R;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class NotificationsListAdapter extends RecyclerView.Adapter<NotificationsListAdapter.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    NotificationsListAdapter(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.fragment_notification_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        String user = mData.get(position);
+        holder.myTextView.setText(user + " has requested to be your friend");
+        holder.acceptButton.setVisibility(View.INVISIBLE);
+        holder.denyButton.setVisibility(View.VISIBLE);
+
+    }
+
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+        ImageView acceptButton;
+        ImageView denyButton;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.Text);
+            acceptButton = itemView.findViewById(R.id.accept_tick);
+            denyButton = itemView.findViewById(R.id.deny_cross);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    String getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
