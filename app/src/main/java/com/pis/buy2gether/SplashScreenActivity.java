70d987b787f82e7eb1bf1bb344f1cdd0ecbe1253
ChenghengLi <li471115381@gmail.com>
2022-04-09 20:11:13 +0200

    New version of the Splash Screen. Adapting and implementing the product view to the current project.

--- app/src/main/java/com/pis/buy2gether/SplashScreenActivity.java
+++ app/src/main/java/com/pis/buy2gether/SplashScreenActivity.java
@@ -3,28 +3,47 @@ package com.pis.buy2gether;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.airbnb.lottie.LottieAnimationView;
-import com.pis.buy2gether.databinding.ActivityMainBinding;
-import com.pis.buy2gether.databinding.ActivitySplashScreenBinding;
+
+import com.pis.buy2gether.activities.Product_view;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private ActivitySplashScreenBinding binding;
+    private static int SPLASH_TIME_OUT = 1500;
+    private Animation topAnim, bottomAnim;
+    private ImageView image;
+    private TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
-        Handler handler = new Handler();
-        handler.postDelayed(r,3000);
 
+
+        //Animation
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.shopping);
+        name = findViewById(R.id.name);
+
+        //Set animation
+        image.setAnimation(bottomAnim);
+        name.setAnimation(topAnim);
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }, SPLASH_TIME_OUT);
     }
 
-    final Runnable r = new Runnable() {
-        public void run() {
-            Intent startApp = new Intent(SplashScreenActivity.this,MainActivity.class);
-            startActivity(startApp);
-            finish();
-        }
-    };
+
 }
---
+++ app/src/main/java/com/pis/buy2gether/ui/login/Login.java
@@ -0,0 +1,15 @@
+package com.pis.buy2gether.ui.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.pis.buy2gether.R;
+
+public class Login extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+    }
+}
