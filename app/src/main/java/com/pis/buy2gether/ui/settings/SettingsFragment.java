ac9f8f21c0075efe321e4e08d414d6f3f4f09284
alba-ch <albachgarrido@gmail.com>
2022-04-11 00:20:13 +0200

    Implemented return from Settings view

--- app/src/main/java/com/pis/buy2gether/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/pis/buy2gether/ui/settings/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
@@ -24,18 +25,27 @@ public class SettingsFragment extends Fragment {
     private FragmentSettingsBinding binding;
 
     ImageButton btn_return;
+    ImageButton btn_change_psw;
+    ImageButton btn_edit_pfp;
+    EditText edit_username;
+    EditText edit_city;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings,container,false);
         btn_return = view.findViewById(R.id.btn_return);
+        btn_change_psw = view.findViewById(R.id.btn_change_psw);
+        btn_edit_pfp = view.findViewById(R.id.btn_edit_pfp);
+        edit_username = view.findViewById(R.id.edit_username);
+        edit_city = view.findViewById(R.id.edit_city);
+
         btn_return.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getActivity(),"RETURN",Toast.LENGTH_SHORT).show();
-                /* Canviem de fragment al que conté la llista d'amics */
+                /* Canviem de fragment a user options */
                 FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
                 fragmentTransaction.replace(R.id.settings, new UserFragment());
-                fragmentTransaction.addToBackStack("settings").commit();
+                fragmentTransaction.commit();
             }
         });
         return view;
@@ -46,17 +56,4 @@ public class SettingsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-/*
-    @Override
-    public void onClick(View view) {
-        boolean process = view.getId() != R.id.shareDummy;
-        switch (view.getId()) {
-            case R.id.btn_return:
-                Toast.makeText(getActivity(), "RETURN", Toast.LENGTH_SHORT).show();
-                getParentFragmentManager().beginTransaction().replace(R.id.settings, new UserFragment()).commit();
-                break;
-            default:
-                break;
-        }
-    }*/
 }
