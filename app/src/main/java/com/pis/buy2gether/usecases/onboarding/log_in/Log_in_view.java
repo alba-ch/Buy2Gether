180529ca497d92d51c25cc1e3f4840f1cd082da3
alba-ch <albachgarrido@gmail.com>
2022-04-13 22:57:58 +0200

    Implemented Log in with email and password using Firebase

--- app/src/main/java/com/pis/buy2gether/usecases/onboarding/log_in/Log_in_view.java
+++ app/src/main/java/com/pis/buy2gether/usecases/onboarding/log_in/Log_in_view.java
@@ -1,6 +1,9 @@
 package com.pis.buy2gether.usecases.onboarding.log_in;
 
+import android.view.animation.CycleInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageButton;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,6 +13,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.pis.buy2gether.usecases.home.ProviderType;
 import com.pis.buy2gether.usecases.onboarding.sign_in.Register_view;
 import com.pis.buy2gether.usecases.home.MainActivity;
 import com.pis.buy2gether.R;
@@ -19,6 +25,9 @@ public class Log_in_view extends AppCompatActivity {
     private Button sign;
     private TextView name;
     private ImageView image;
+    private TextInputEditText userEditText;
+    private TextInputEditText pswEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +38,25 @@ public class Log_in_view extends AppCompatActivity {
         log = findViewById(R.id.btn_login);
         sign = findViewById(R.id.login);
 
+        userEditText = findViewById(R.id.txtin_username);
+        pswEditText = findViewById(R.id.txtin_psw);
+
         name = findViewById(R.id.img_appname);
         image = findViewById(R.id.img_logo);
 
         log.setOnClickListener(v -> {
-            // Check data
-            Intent i;
-            i = new Intent(Log_in_view.this, MainActivity.class);
-            startActivity(i);
-            finish();
+            if(!(userEditText.getText().toString().isEmpty()) && !(pswEditText.getText().toString().isEmpty())){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(userEditText.getText().toString(),pswEditText.getText().toString()).addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        showHome(task.getResult().getUser().getEmail(), ProviderType.BASIC);
+                    }else{
+                        showAlert();
+                    }
+                });
+            }else{
+                userEditText.startAnimation(shakeError());
+                pswEditText.startAnimation(shakeError());
+            }
         } );
 
         sign.setOnClickListener(v -> {
@@ -53,6 +72,27 @@ public class Log_in_view extends AppCompatActivity {
             startActivity(i);
             finish();
         } );
+    }
 
+    private void showAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage("S'ha produït un error autentificant l'usuari.");
+        builder.setPositiveButton("Acceptar",null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void showHome(String email, ProviderType provider){
+        Intent i = new Intent(Log_in_view.this, MainActivity.class).putExtra("provider",provider.name()).putExtra("email",email);
+        startActivity(i);
+        finish();
+    }
+
+    public TranslateAnimation shakeError() {
+        TranslateAnimation shake = new TranslateAnimation(0, 0, 0, 20);
+        shake.setDuration(1000);
+        shake.setInterpolator(new CycleInterpolator(7));
+        return shake;
     }
 }
