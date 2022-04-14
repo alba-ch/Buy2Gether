8e932c1e2b597083e1e7927435420a5868b7a493
alba-ch <albachgarrido@gmail.com>
2022-04-15 01:50:36 +0200

    Added FirebaseRDBService (Provider)

--- app/src/main/java/com/pis/buy2gether/model/session/Session.java
+++ app/src/main/java/com/pis/buy2gether/model/session/Session.java
@@ -2,13 +2,20 @@ package com.pis.buy2gether.model.session;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.pis.buy2gether.R;
 import com.pis.buy2gether.provider.ProviderType;
 import com.pis.buy2gether.provider.preferences.PreferencesProvider;
+import com.pis.buy2gether.provider.services.FirebaseRDBService;
+
+import java.util.HashMap;
 
 /* Class for user data persistence and write/read database info (only user session data)*/
 public enum Session {
     INSTANCE;
+
+    private FirebaseRDBService RDB = FirebaseRDBService.INSTANCE;
 
     public String getDataSession(Context context, String key){
         return PreferencesProvider.string(context, key);
@@ -20,6 +27,18 @@ public enum Session {
 
     public void clearDataSession(Context context){
         PreferencesProvider.clear(context);
+    }
+
+    public void saveDB(String coll, String doc, HashMap data){
+        RDB.save(coll,doc,data);
+    }
+
+    public void deleteDB(String coll, String doc){
+        RDB.delete(coll,doc);
+    }
+
+    public Task<DocumentSnapshot> getDB(String coll, String doc){
+        return RDB.get(coll,doc);
     }
 
 }
---
+++ app/src/main/java/com/pis/buy2gether/provider/services/FirebaseRDBService.java
@@ -0,0 +1,32 @@
+package com.pis.buy2gether.provider.services;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+/* Write or read data from Firebase Database */
+public enum FirebaseRDBService {
+    INSTANCE;
+
+    public void save(String collectionPath, String doc, HashMap data){
+        db().collection(collectionPath).document(doc).set(data);
+    }
+
+    private void save(String collectionPath, String doc, String data){
+        db().collection(collectionPath).document(doc).set(data);
+    }
+
+    public void delete(String collectionPath, String doc){
+        db().collection(collectionPath).document(doc).delete();
+    }
+
+    public Task<DocumentSnapshot> get(String collectionPath, String doc){
+        return db().collection(collectionPath).document(doc).get();
+    }
+
+    public FirebaseFirestore db(){
+        return FirebaseFirestore.getInstance();
+    }
+}
